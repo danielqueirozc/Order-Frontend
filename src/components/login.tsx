0e5989fe61b3877622715c5fd75fd74496ac835f
danielqueirozc <danielqueiroz.job@gmail.com>
2024-12-22 16:31:47 -0300

    feat: add login component

---
+++ src/components/login.tsx
@@ -0,0 +1,14 @@
+export function Login() {
+    return (
+        <div>
+            <h1>Login to your account</h1>
+            <div>
+
+            </div>
+
+            <form>
+                
+            </form>
+        </div>
+    )
+}
